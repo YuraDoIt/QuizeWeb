08a7201f773b3872c586d598d421efa7e4c2d09e
Yurii <yura9989@gmail.com>
2021-03-07 00:53:50 +0200

    javascriptFile added for quize

---
+++ quize.js
@@ -0,0 +1,9 @@
+const quizContainer = document.getElementById('quiz');
+const resultContainer = document.getElementById('results');
+const submintButton = document.getElementById('submit');
+
+function buildQuiz(){}
+
+function showResult(){}
+
+buildQuiz();
