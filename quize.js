81d37804b2fdd6bf9ecc08c3a7478ed667ed3ca5
Yurii <yura9989@gmail.com>
2021-03-07 14:32:22 +0200

    quize js changed

--- quize.js
+++ quize.js
@@ -2,8 +2,56 @@ const quizContainer = document.getElementById('quiz');
 const resultContainer = document.getElementById('results');
 const submintButton = document.getElementById('submit');
 
-function buildQuiz(){}
+function buildQuiz(){
+    const output = [];
+
+    myQuestion.forEach(
+        (currentQuestion, questionNumber) => {
+            const answer = [];
+
+            for(letter in currentQuestion.answers){
+
+                answers.push(
+                    `<label>
+                    </label>`
+                )
+            }
+        }
+    )
+}
 
 function showResult(){}
 
 buildQuiz();
+
+submintButton.addEventListener('click', showResults);
+
+const myQuestion = [
+    {
+        question: "Who was founder of JavaScript?",
+        answers:{
+            a: "Some pizduk",
+            b: "Sasha Bolvan",
+            c: "Brendan Eich"
+        },
+        correctAnswer: "c"        
+    },
+    {
+        question: "When Lviv was created ?",
+        answers:{
+            a: 1000,
+            b: 1600,
+            c: 1256
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which one of these is a JavaScript manager package ?",
+        answers:{
+            a: "Node.js",
+            b: "npm",
+            c: "npmScript"
+        },
+        correctAnswer: "b"
+    }
+];
